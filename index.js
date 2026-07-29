cac3671bac8ea8f3ee15fe1ccd42f8723fdc6e9a
pcodegame-hash <pcodegame@gmail.com>
2026-07-29 23:44:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ import forumRoutes from "./routes/forumRoutes.js";
 import { setServers } from "node:dns/promises";
 setServers(["8.8.8.8", "1.1.1.1"]); // Uses Google and Cloudflare public DNS
 
-const app = express();
+const app = express
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/codegame";
 
